640384a65ab9b98cdebe28d8c8eb2a0c4941ae3f
Mario Lazzari <mario.lazzari@gmail.com>
2023-06-06 22:40:40 +0200

    Done lesson 2

--- 02_remove_union.ts
+++ 02_remove_union.ts
@@ -2,5 +2,11 @@ export type Letters = "a" | "b" | "c";
 type RemoveC<TType> = TType extends "c" ? never : TType;
 type WowWithoutC = RemoveC<Letters>;
 
+type AddD<TType> = TType extends "c" ? "d" : TType;
+type WowWithD = AddD<Letters>;
+
 const a: WowWithoutC = "a";
 console.log(a);
+
+const d: WowWithD = "d";
+console.log(d);
